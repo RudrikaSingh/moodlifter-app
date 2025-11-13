78131ec522911ee07ff20d67a780daf7a58493a7
RudrikaSingh <rudrikasingh04@gmail.com>
2025-11-13 17:23:21 +0530

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ except Exception:
     st.error("API key not found! Please add it to your Streamlit secrets.")
     st.stop()
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Streamlit UI
 st.set_page_config(page_title="MoodLifter", page_icon="🌟")
